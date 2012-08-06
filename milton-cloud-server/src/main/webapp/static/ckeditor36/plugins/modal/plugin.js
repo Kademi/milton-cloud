cd82a695bd3ceba850cb0db6f4489746177f2510
Bob khin <ducdhm@gmail.com>
2012-08-06 15:44:54 +0700

    Update modal plugin

--- milton-cloud-server/src/main/webapp/static/ckeditor36/plugins/modal/plugin.js
+++ milton-cloud-server/src/main/webapp/static/ckeditor36/plugins/modal/plugin.js
@@ -1,5 +1,5 @@
 (function(CKEDITOR) {
-var defaults_width = 350;
+var required_string = '<sup style="color: #ff4000">*</sup>';
 
 CKEDITOR.plugins.add('modal', {
 	init: function(editor) {
@@ -9,9 +9,36 @@ CKEDITOR.plugins.add('modal', {
 			label: 'Insert modal',
 			command: 'modalLinkDialog',
 			icon: this.path + 'images/modal.png'
-		});		
+		});
+		
+		editor.on( 'doubleclick', function( evt ) {
+			var element = CKEDITOR.plugins.link.getSelectedLink( editor ) || evt.data.element;
+
+			if(!element.isReadOnly()) {
+				if (element.is('a') && element.$.className === 'anchor-modal') {
+					evt.data.dialog = 'modalLinkDialog';
+					editor.getSelection().selectElement(element);
+				}
+			}
+		});
 		
 		CKEDITOR.dialog.add('modalLinkDialog', function(editor) {
+			var default_width = 350;
+			
+			var parseModalLink = function(editor, element) {
+				var href = element.getAttribute('href').replace('#',''),
+					modal = editor.document.getById(href);
+				
+				this._.selectedElement = element;
+				
+				return {
+					text: element.getHtml(),
+					content: modal.getHtml(),
+					width: modal.getStyle('width').replace('px', ''),
+					height: modal.getStyle('height').replace('px', '')
+				};
+			};
+			
 			return {
 				title: 'Modal Properties',
 				minWidth: 400,
@@ -22,18 +49,28 @@ CKEDITOR.plugins.add('modal', {
 					elements: [{
 						type: 'text',
 						id: 'text',
-						label: 'Displayed Text<sup style="color: #ff4000">*</sup>',
+						label: 'Displayed Text' + required_string,
 						validate: CKEDITOR.dialog.validate.notEmpty('The Displayed Text cannot be empty!'),
 						required: true,
+						setup: function(data) {
+							if(data.text) {
+								this.setValue(data.text);
+							}
+						},
 						commit: function(data) {
 							data.text = this.getValue();
 						}
 					}, {
 						type: 'textarea',
 						id: 'content',
-						label: 'Modal Content<sup style="color: #ff4000">*</sup>',
+						label: 'Modal Content' + required_string,
 						validate: CKEDITOR.dialog.validate.notEmpty('The Modal Content cannot be empty!'),
 						required: true,
+						setup: function(data) {
+							if(data.content) {
+								this.setValue(data.content);
+							}
+						},
 						commit: function(data) {
 							data.content = this.getValue();
 						}
@@ -41,6 +78,11 @@ CKEDITOR.plugins.add('modal', {
 						type: 'text',
 						id: 'width',
 						label: 'Width',
+						setup: function(data) {
+							if(data.width) {
+								this.setValue(+data.width + 50); // Add 50px of padding left and right
+							}
+						},
 						validate: function() {
 							var value = this.getValue();
 							
@@ -58,6 +100,11 @@ CKEDITOR.plugins.add('modal', {
 						type: 'text',
 						id: 'height',
 						label: 'Height',
+						setup: function(data) {
+							if(data.height) {
+								this.setValue(data.height);
+							}
+						},
 						validate: function() {
 							var value = this.getValue();
 							
@@ -73,46 +120,97 @@ CKEDITOR.plugins.add('modal', {
 						}
 					}]
 				}],
-				onOk: function() {
-					// Create a link element and an object that will store the data entered in the dialog window.
-					// http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.dom.document.html#createElement
-					var dialog = this,
-						data = {},
-						id = 'modal_' + Math.round(Math.random() * 1000000).toString() + '_' + Date.now(),
-						link = editor.document.createElement('a'),
-						div = editor.document.createElement('div');
+				onShow: function() {
+					var editor = this.getParentEditor(),
+						selection = editor.getSelection(),
+						element = null;
 					
-					this.commitContent(data);
-					
-					link.setHtml(data.text);	
-					link.setAttributes({
-						'href': '#' + id,
-						'class': 'anchor-modal'
-					});
-					
-					var style = 'display: none;',
-						width = (data.width || defaults_width) - 50; // Subtract 25px from padding left and right
-					
-					style += 'width: ' + width + 'px;';
-					
-					if(data.height) {
-						style += 'height: ' + data.height + 'px;';
+					if((element = CKEDITOR.plugins.modalLink.getSelectedLink(editor)) && element.hasAttribute('href')) {
+						selection.selectElement( element );
+					} else {
+						element = null;		
 					}
 					
-					div.setHtml(data.content);	
-					div.setAttributes({
-						'id': id,
-						'style': style,
-						'class': 'linked-modal'
-					});
+					if(element) {					
+						this.setupContent(parseModalLink.apply(this,[editor,element]));
+					}					
+				},
+				onOk: function() {
+					var dialog = this,
+						data = {},
+						id;						
 					
-					editor.insertElement(link);
-					editor.insertElement(div);
+					this.commitContent(data);					
+					
+					if(this._.selectedElement) {
+						var target = this._.selectedElement,
+							modal = editor.document.getById(target.getAttribute('href').replace('#', '')),
+							style = 'display: none;',
+							width = (data.width || this.defaults.width) - 50; // Subtract 25px from padding left and right
+						
+						style += 'width: ' + width + 'px;';
+						
+						if(data.height) {
+							style += 'height: ' + data.height + 'px;';
+						}
+						
+						// Set html for anchor
+						target.setHtml(data.text);
+						
+						modal.setHtml(data.content);
+						modal.setAttribute('style', style);						
+					} else {					
+						var link = editor.document.createElement('a'),
+							div = editor.document.createElement('div');
+							
+						id = 'modal_' + Math.round(Math.random() * 1000000).toString() + '_' + Date.now();
+							
+						link.setHtml(data.text);	
+						link.setAttributes({
+							'href': '#' + id,
+							'class': 'anchor-modal'
+						});
+						
+						var style = 'display: none;',
+							width = (data.width || this.defaults.width) - 50; // Subtract 25px from padding left and right						
+						style += 'width: ' + width + 'px;';						
+						if(data.height) {
+							style += 'height: ' + data.height + 'px;';
+						}						
+						div.setHtml(data.content);	
+						div.setAttributes({
+							'id': id,
+							'style': style,
+							'class': 'linked-modal'
+						});
+						
+						editor.insertElement(link);
+						editor.insertElement(div);
+					}
 				}
 			};
 		} );
 	}
 });
+
+CKEDITOR.plugins.modalLink = {
+	getSelectedLink: function(editor) {
+		try {
+			var selection = editor.getSelection();
+			if ( selection.getType() == CKEDITOR.SELECTION_ELEMENT ) {
+				var selectedElement = selection.getSelectedElement();
+				if ( selectedElement.is( 'a' ) && selectedElement.$.className === 'anchor-modal' )
+					return selectedElement;
+			}
+
+			var range = selection.getRanges( true )[ 0 ];
+			range.shrink( CKEDITOR.SHRINK_TEXT );
+			var root = range.getCommonAncestor();
+			return root.getAscendant( 'a', true );
+		}
+		catch( e ) { return null; }
+	}
+}
 
 
 }(CKEDITOR));
