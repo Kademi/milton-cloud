dccffdb65bb75a4ab1a2718e7a1651cd24135f41
Bob khin <ducdhm@gmail.com>
2012-08-15 16:24:21 +0700

    update

--- milton-cloud-server/src/main/webapp/static/scratch/modal/modal.js
+++ milton-cloud-server/src/main/webapp/static/scratch/modal/modal.js
@@ -11,7 +11,17 @@
         $('a.close-modal').click(function(e) {
             $.tinybox.close();
         });
+		
+		$('.modal-table thead th:last-child').addClass('last');
+		$('.modal-table tbody tr')
+			.eq(0)
+				.addClass('first')
+			.end()
+			.eq(1)
+				.addClass('second')
+			.end()
+			.eq(2)
+				.addClass('third');
     });
-
 
 }(jQuery));
