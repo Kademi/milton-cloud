ca34ecb5e394765370c31fa73c69d0e4ffe1de39
Brad Mac <brad@mcevoysoftware.co.nz>
2017-11-03 15:49:37 +1300

    1.3.8 - FileSystemWatchingService

---
+++ milton-cloud-sync/src/main/java/io/milton/sync/triplets/FileSystemWatchingService.java
@@ -0,0 +1,151 @@
+package io.milton.sync.triplets;
+
+import io.milton.sync.Utils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.Watchable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author brad
+ */
+public class FileSystemWatchingService {
+
+    private static final Logger log = LoggerFactory.getLogger(FileSystemWatchingService.class);
+
+    private static final WatchEvent.Kind<?>[] events = {StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY};
+
+    private final WatchService watchService;
+    private final ScheduledExecutorService scheduledExecutorService;
+    private final Map<WatchNotificationListener, File> mapOfListeners;
+    private ScheduledFuture<?> futureScan;
+
+    public FileSystemWatchingService(WatchService watchService, ScheduledExecutorService scheduledExecutorService) {
+        this.watchService = watchService;
+        this.mapOfListeners = new HashMap<>();
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+
+    public void start() {
+
+        Runnable rScan = () -> {
+            try {
+                scanFsEvents();
+            } catch (IOException ex) {
+                log.error("Exception processing events", ex);
+            }
+        };
+        log.trace("Begin file watch loop");
+        futureScan = scheduledExecutorService.scheduleWithFixedDelay(rScan, 200, 200, TimeUnit.MILLISECONDS);
+    }
+
+    public void stop() {
+        if (futureScan != null) {
+            futureScan.cancel(true);
+        }
+    }
+
+    private void scanFsEvents() throws IOException {
+        WatchKey watchKey;
+        watchKey = watchService.poll(); // this call is blocking until events are present
+        if (watchKey == null) {
+            return;
+        }
+
+        Watchable w = watchKey.watchable();
+        java.nio.file.Path watchedPath = (java.nio.file.Path) w;
+        // poll for file system events on the WatchKey
+
+        for (final WatchEvent<?> event : watchKey.pollEvents()) {
+            // log.info("scanFsEvents: " + System.currentTimeMillis());
+            WatchEvent.Kind<?> kind = event.kind();
+            java.nio.file.Path p = (java.nio.file.Path) event.context();
+            java.nio.file.Path pathCreated = (java.nio.file.Path) event.context();
+            final File f = new File(watchedPath + File.separator + pathCreated);
+
+            for (Map.Entry<WatchNotificationListener, File> entry : mapOfListeners.entrySet()) {
+                File dir = entry.getValue();
+                if (f.getAbsolutePath().startsWith(dir.getAbsolutePath())) {
+                    entry.getKey().onChange(kind, f);
+                }
+            }
+
+            // If this is a new directory we need to add a watch for it
+            if (kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
+                if (f.isDirectory()) {
+                    registerWatchDir(f);
+                }
+            } else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                if (f.isDirectory()) {
+                    unregisterWatchDir(f);
+                }                
+            }
+        }
+
+        // if the watched directed gets deleted, get out of run method
+        if (!watchKey.reset()) {
+            log.info("Watch is no longer valid");
+            watchKey.cancel();
+        }
+    }
+
+    public void watch(File dir, WatchNotificationListener listener) throws IOException {
+        mapOfListeners.put(listener, dir);
+        initWatch(dir);
+    }
+
+    private void initWatch(File dir) throws IOException {
+        if (dir.isDirectory()) {
+            if (Utils.ignored(dir)) {
+                return;
+            }
+            registerWatchDir(dir);
+            File[] list = dir.listFiles();
+            if (list != null) {
+                for (File f : list) {
+                    initWatch(f);
+                }
+            }
+        }
+    }
+
+    private void registerWatchDir(final File dir) throws IOException {
+        if (watchService == null) {
+            return;
+        }
+
+        final java.nio.file.Path path = FileSystems.getDefault().getPath(dir.getAbsolutePath());
+
+        WatchKey key = path.register(watchService, events);
+        //mapOfWatchKeysByDir.put(dir, key);
+        log.info("Watching: " + path);
+
+    }
+    
+    private void unregisterWatchDir(final File dir) {
+
+    }    
+
+    public ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
+    }
+    
+    
+
+    public interface WatchNotificationListener {
+
+        void onChange(WatchEvent.Kind<?> event, File changed);
+    }
+}
--- milton-cloud-sync/src/main/java/io/milton/sync/triplets/MemoryLocalTripletStore.java
+++ milton-cloud-sync/src/main/java/io/milton/sync/triplets/MemoryLocalTripletStore.java
@@ -3,10 +3,8 @@ package io.milton.sync.triplets;
 import java.io.*;
 import java.nio.file.*;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import org.hashsplit4j.api.BlobStore;
 import org.hashsplit4j.api.Parser;
@@ -16,7 +14,6 @@ import io.milton.sync.Utils;
 import io.milton.sync.event.EventUtils;
 import io.milton.sync.event.FileChangedEvent;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -35,15 +32,15 @@ public class MemoryLocalTripletStore {
     private static final WatchEvent.Kind<?>[] events = {StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY};
 
     private final File root;
-    private WatchService watchService;
+    private final FileSystemWatchingService fileSystemWatchingService;
+    private final ScheduledExecutorService scheduledExecutorService;
     private final EventManager eventManager;
     private final BlobStore blobStore;
     private final HashStore hashStore;
     private final RepoChangedCallback callback;
     private final Consumer<Runnable> filter;
     private boolean initialScanDone;
-    private ScheduledFuture<?> futureScan;
-    private ScheduledExecutorService scheduledExecutorService;
+
     private final HashCalc hashCalc = HashCalc.getInstance();
     private boolean paused; // if true ignores fs events
 
@@ -51,7 +48,7 @@ public class MemoryLocalTripletStore {
     private long logTime;
 
     public MemoryLocalTripletStore(File root, BlobStore blobStore, HashStore hashStore) throws IOException {
-        this(root, null, blobStore, hashStore, null, null, null, null, null);
+        this(root, null, blobStore, hashStore, null, null, null, null);
     }
 
     /**
@@ -63,21 +60,27 @@ public class MemoryLocalTripletStore {
      * @param callback
      * @param filter
      * @param dataDir
-     * @param watchService
-     * @param scheduledExecutorService
+     * @param fileSystemWatchingService
      * @throws IOException
      */
-    public MemoryLocalTripletStore(File root, EventManager eventManager, BlobStore blobStore, HashStore hashStore, RepoChangedCallback callback, 
-            Consumer<Runnable> filter, File dataDir, WatchService watchService, ScheduledExecutorService scheduledExecutorService) throws IOException {
+    public MemoryLocalTripletStore(File root, EventManager eventManager, BlobStore blobStore, HashStore hashStore, RepoChangedCallback callback,
+            Consumer<Runnable> filter, File dataDir, FileSystemWatchingService fileSystemWatchingService) throws IOException {
         this.root = root;
         this.blobStore = blobStore;
         this.hashStore = hashStore;
         this.callback = callback;
         this.eventManager = eventManager;
         this.filter = filter;
-        
-        this.watchService = watchService;
-        this.scheduledExecutorService = scheduledExecutorService;        
+
+        if (fileSystemWatchingService == null) {
+            scheduledExecutorService = Executors.newScheduledThreadPool(1);
+            final java.nio.file.Path path = FileSystems.getDefault().getPath(root.getAbsolutePath());
+            WatchService watchService = path.getFileSystem().newWatchService();
+            this.fileSystemWatchingService = new FileSystemWatchingService(watchService, scheduledExecutorService);
+        } else {
+            this.fileSystemWatchingService = fileSystemWatchingService;
+            this.scheduledExecutorService = fileSystemWatchingService.getScheduledExecutorService();
+        }
 
         ICompositeCacheAttributes cfg = new CompositeCacheAttributes();
         cfg.setUseDisk(true);
@@ -125,47 +128,23 @@ public class MemoryLocalTripletStore {
 
     /**
      * Start processing file system events
+     *
+     * @throws java.io.IOException
      */
     public void start() throws IOException {
-        if (scheduledExecutorService == null) {
-            scheduledExecutorService = Executors.newScheduledThreadPool(1);
-        }
-        final java.nio.file.Path path = FileSystems.getDefault().getPath(root.getAbsolutePath());
-        if (watchService == null) {
-            watchService = path.getFileSystem().newWatchService();
-        }
 
-        initWatch(root);
+        this.fileSystemWatchingService.watch(root, (WatchEvent.Kind<?> event, File changed) -> {
+            processEvent(changed, event);
+        });
+        this.fileSystemWatchingService.start();
 
-        // after initial scan is done, start the thread which will process file changed events
-        // note these events begin accumulating as the scan processes directories
-        Runnable rScan = () -> {
-            try {
-                scanFsEvents();
-            } catch (IOException ex) {
-                log.error("Exception processing events", ex);
-            }
-        };
-        log.trace("Begin file watch loop: " + root.getAbsolutePath());
-        futureScan = scheduledExecutorService.scheduleWithFixedDelay(rScan, 200, 200, TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * Stop processing file system events
-     */
-    public void stop() {
-        if (futureScan != null) {
-            futureScan.cancel(true);
-        }
     }
 
     public String scanDirectory(File dir) throws IOException {
         if (Utils.ignored(dir)) {
             return null;
         }
-        if (!initialScanDone) {
-            registerWatchDir(dir);
-        }
+
         if (System.currentTimeMillis() - logTime > 2000) { // just output current dir every couple of seconds
             log.trace("scanDirectory: dir={}", dir.getAbsolutePath());
             logTime = System.currentTimeMillis();
@@ -236,115 +215,50 @@ public class MemoryLocalTripletStore {
         return hash;
     }
 
-    private final Map<File, WatchKey> mapOfWatchKeysByDir = new HashMap<>();
-
-    private void registerWatchDir(final File dir) throws IOException {
-        if (watchService == null) {
-            return;
-        }
-        if (mapOfWatchKeysByDir.containsKey(dir)) {
-            WatchKey key = mapOfWatchKeysByDir.get(dir);
-            key.cancel();
-            mapOfWatchKeysByDir.remove(dir);
-        }
-        final java.nio.file.Path path = FileSystems.getDefault().getPath(dir.getAbsolutePath());
-        // will only watch specified directory, not subdirectories
-        WatchKey key = path.register(watchService, events);
-        mapOfWatchKeysByDir.put(dir, key);
-        //log.info("Now watching: " + dir.getAbsolutePath());
-    }
-
-    private void unregisterWatchDir(final File dir) {
-        WatchKey key = mapOfWatchKeysByDir.get(dir);
-        if (key != null) {
-            log.info("Cancel watch: " + dir.getAbsolutePath() + " - " + key.watchable());
-            key.cancel();
-        }
-
-    }
-
-    private int queuedEvents;
-    private long lastEventTime;
-
-    private void scanFsEvents() throws IOException {
-        WatchKey watchKey;
-        watchKey = watchService.poll(); // this call is blocking until events are present
-        if (watchKey == null) {
-            return;
-        }
-
-        Watchable w = watchKey.watchable();
-        java.nio.file.Path watchedPath = (java.nio.file.Path) w;
-        // poll for file system events on the WatchKey
-
-        for (final WatchEvent<?> event : watchKey.pollEvents()) {
-            // log.info("scanFsEvents: " + System.currentTimeMillis());
-            WatchEvent.Kind<?> kind = event.kind();
-            java.nio.file.Path p = (java.nio.file.Path) event.context();
-            if (p.toString().endsWith(".spliffy") || p.toString().endsWith(Syncer.TMP_SUFFIX)) {
-                //ignore
+    private void processEvent(File f, WatchEvent.Kind<?> kind) {
+        String changedPath = f.getAbsolutePath();
+        if (changedPath.endsWith(".spliffy") || changedPath.endsWith(Syncer.TMP_SUFFIX)) {
+            //ignore
+        } else {
+            if (paused) {
+                log.trace("Ignoring fs events while paused during scan");
             } else {
-                if (paused) {
-                    log.trace("Ignoring fs events while paused during scan");
-                } else {
-                    if (kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
-                        java.nio.file.Path pathCreated = (java.nio.file.Path) event.context();
+                if (kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
 
-                        final File f = new File(watchedPath + File.separator + pathCreated);
-                        if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
-                            // ignore it
+                    if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
+                        // ignore it
+                    } else {
+                        if (f.isDirectory()) {
+                            directoryCreated(f);
                         } else {
-                            log.trace("scanFsEvents: watchedPath=" + watchedPath);
-                            if (f.isDirectory()) {
-                                directoryCreated(f);
-                            } else {
-                                fileCreated(f);
-                            }
+                            fileCreated(f);
                         }
-                    } else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
-                        java.nio.file.Path pathDeleted = (java.nio.file.Path) event.context();
-                        final File f = new File(watchedPath + File.separator + pathDeleted);
-                        if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
-                            log.trace("ignoring change to ignored file");
-                        } else {
-                            fileDeleted(f);
-                        }
-                    } else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
-                        java.nio.file.Path pathDeleted = (java.nio.file.Path) event.context();
-                        final File f = new File(watchedPath + File.separator + pathDeleted);
-                        if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
-                            // ignored
-                        } else {
-                            fileDeleted(f);
-                        }
-                    } else if (kind.equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
-                        java.nio.file.Path pathModified = (java.nio.file.Path) event.context();
-                        final File f = new File(watchedPath + File.separator + pathModified);
-                        if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
-                            log.trace("ignoring change to ignored file");
-                        } else {
-                            fileModified(f);
-                        }
+                    }
+                } else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                    if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
+                        log.trace("ignoring change to ignored file");
+                    } else {
+                        fileDeleted(f);
+                    }
+                } else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                    if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
+                        // ignored
+                    } else {
+                        fileDeleted(f);
+                    }
+                } else if (kind.equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+                    if (Utils.ignored(f) || Utils.ignored(f.getParentFile())) {
+                        log.trace("ignoring change to ignored file");
+                    } else {
+                        fileModified(f);
                     }
                 }
             }
-        }
-
-        // if the watched directed gets deleted, get out of run method
-        if (!watchKey.reset()) {
-            log.info("Watch is no longer valid");
-            watchKey.cancel();
         }
     }
 
     private void directoryCreated(final File f) {
         log.info("Directory Created: " + f.getAbsolutePath());
-        try {
-            registerWatchDir(f);
-            scanDir(f.getParentFile()); // scan the parent, so it can see the new member
-        } catch (IOException e) {
-            log.error("Exception in directoryCreated", e);
-        }
     }
 
     private void fileCreated(File f) {
@@ -359,11 +273,12 @@ public class MemoryLocalTripletStore {
 
     private void fileDeleted(File f) {
         log.info("file deleted " + f.getAbsolutePath());
-        unregisterWatchDir(f); // f might be a file or directory, but unregister checks for presence so ok to call regardless
         scanDir(f.getParentFile());
     }
 
     private final Set<File> scanningDirs = new HashSet<>();
+    private int queuedEvents;
+    private long lastEventTime;
 
     /**
      *
@@ -433,21 +348,6 @@ public class MemoryLocalTripletStore {
 
     public File getRoot() {
         return root;
-    }
-
-    private void initWatch(File dir) throws IOException {
-        if (dir.isDirectory()) {
-            if (Utils.ignored(dir)) {
-                return;
-            }
-            registerWatchDir(dir);
-            File[] list = dir.listFiles();
-            if (list != null) {
-                for (File f : list) {
-                    initWatch(f);
-                }
-            }
-        }
     }
 
     public interface RepoChangedCallback {
